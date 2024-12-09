730ae4a916db19f4d4e190a67937a9500a4eb94e
Dasonchris <dasonchris47@gmail.com>
2024-12-09 11:44:43 +0000

    done

--- Script.JS/script.js
+++ Script.JS/script.js
@@ -20,3 +20,12 @@ var swiper = new Swiper(".review-slider", {
     },
 });
 
+
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+ function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+ };
